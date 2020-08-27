e4e026db16a4a2059b3a1b1312292828836f10fc
roof558 <70292916+roof558@users.noreply.github.com>
2020-08-26 18:56:04 -0700

    Update HelloWord.java

--- HelloWord.java
+++ HelloWord.java
@@ -1,4 +1,9 @@
 
 public class HelloWord {
-
+  
+  public static void tempChange(double faren){
+    double celc = 5 * (faren - 32) / 9;
+    
+    System.out.println("Farenheit: " + faren + "\n Celecus: " + celc);
+  }
 }
