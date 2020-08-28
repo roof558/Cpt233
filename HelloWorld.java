59e91159fb51478386bc08b8639e18375b46fa1e
roof558 <roof558@gmail.com>
2020-08-28 13:35:57 -0700

    Change name

---
+++ HelloWorld.java
@@ -0,0 +1,10 @@
+
+public class HelloWorld {
+  
+  public static void tempChange(double faren){
+    double celc = 5 * (faren - 32) / 9;
+    
+    System.out.println("Farenheit: " + faren + "\n Celecus: " + celc);
+    
+  }
+}
