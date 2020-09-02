20d3c19b61b3b984e789e1b7e5dbb582ec36f0b1
roof558 <roof558@gmail.com>
2020-09-01 17:22:43 -0700

    adding for java project

---
+++ src/MicroAssignment1/RoomCarpet.java
@@ -0,0 +1,13 @@
+package MicroAssignment1;
+
+/**
+ * 
+ * @author Justin Lee
+ *
+ */
+
+public class RoomCarpet {
+	private RoomCarpet carpet;
+	
+	
+}
---
+++ src/MicroAssignment1/RoomDimension.java
@@ -0,0 +1,15 @@
+package MicroAssignment1;
+/**
+ * 
+ * @author Justin Lee
+ *
+ */
+public class RoomDimension {
+
+	private int length;
+	private int width;
+	
+	public int area() {
+		return length * width;
+	}
+}
