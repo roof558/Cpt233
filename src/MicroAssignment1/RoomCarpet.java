23de9aba54a800305e57e8491841a90e9e8106bd
roof558 <roof558@gmail.com>
2020-09-01 17:52:36 -0700

    Change

--- src/MicroAssignment1/RoomCarpet.java
+++ src/MicroAssignment1/RoomCarpet.java
@@ -1,13 +1,19 @@
 package MicroAssignment1;
 
 /**
+ * RoomCarpet.java
+ * 
  * 
  * @author Justin Lee
- *
+ * @since 09/01/2020
+ * @version 1.0
  */
 
 public class RoomCarpet {
 	private RoomCarpet carpet;
+	private double cost;
 	
-	
+	public double totalCost() {
+		return cost;
+	}
 }
