910860583742c3bd1e669e0c4b3f7a410880071d
roof558 <roof558@gmail.com>
2020-09-01 17:53:00 -0700

    Change

--- src/MicroAssignment1/RoomDimension.java
+++ src/MicroAssignment1/RoomDimension.java
@@ -1,15 +1,47 @@
 package MicroAssignment1;
 /**
+ * RoomDimension.java
+ * Object RoomDimension that contains the instance variable of length and width that used
+ * to calculate the total cost of room carpet.
+ * Object provide the total area of room dimension.
  * 
  * @author Justin Lee
- *
+ * @since 09/01/2020
+ * @version 1.0
  */
 public class RoomDimension {
 
-	private int length;
-	private int width;
+	private double length;
+	private double width;
 	
-	public int area() {
+	/*
+	 * Constructor of class RoomDimension, set instance variable of class.
+	 * 
+	 * @param len Double varaibles that set to be instance variable length.
+	 * @param w Double variable that set to be instance variable width.
+	 */
+	public RoomDimension(double len, double w) {
+		this.length = len;
+		this.width = w;
+	}
+	
+	/*
+	 * Method that calculate the area of room dimension.
+	 * length * width
+	 * 
+	 * @return Double varaible that is area of room.
+	 */
+	public double getArea() {
 		return length * width;
+	}
+	
+	/*
+	 * Override method of toString to print out class info.
+	 * 
+	 * @return String variable to print out the sentence.
+	 */
+	@Override
+	public String toString() {
+		return "The room dimension is " + getArea();
 	}
 }
